0e5fec3c1e3784bce7c008f9bfaf030c8ce14532
Alejandro <alepazte@hotmail.com>
2016-10-10 11:42:56 -0600

    update

--- App 2 - Go Ratchet/js/app.js
+++ App 2 - Go Ratchet/js/app.js
@@ -1,16 +1,20 @@
 var app = angular.module('myApp', ['ngStorage']);
 var controllers = {};
 
-controllers.listaCtrl = function($scope, $http, $localStorage, $sessionStorage, $window) {
+controllers.navCtrl = function($scope, $window) {
+    $scope.goTo = function(path) {
+        $window.location.href = (path + '.html');
+    }
+}
+
+controllers.listaCtrl = function($scope, $http, $sessionStorage, $window) {
     $scope.productos = [];
-    
 
     $http.get("http://pymesv.com/cliente04sw/servicio/ws/producto.php")
 	    .success(function(response) {
             $scope.productos = response;
             console.log(response);
 	    });
-
 
     $scope.setProducto = function(item) {
         $sessionStorage.producto = item;
@@ -20,6 +24,7 @@ controllers.listaCtrl = function($scope, $http, $localStorage, $sessionStorage, 
 
 controllers.productoCtrl = function($scope, $localStorage, $sessionStorage, $window) {
     $scope.producto = {};
+    $scope.cantidad = 1;
     $scope.carrito = [];
 
     if ($sessionStorage.producto != null) {
@@ -30,28 +35,47 @@ controllers.productoCtrl = function($scope, $localStorage, $sessionStorage, $win
         $scope.carrito = $localStorage.carrito;
     }
 
-    $scope.goBack = function() {
-        $window.history.back();
+    $scope.addCarrito = function(item) {
+        if ($scope.cantidad > 0) {
+            $scope.carrito.push({ "item": item, "cantidad": $scope.cantidad });
+            $localStorage.carrito = $scope.carrito;
+            alert("Se ha(n) agregado " + $scope.cantidad + " " + item.nombre + " al carrito correctamente");
+        }
     }
 
-    $scope.addCarrito = function(item, cant) {
-        $scope.carrito.push(item);
-        console.log(item.nombre + " agregado/a al carrito");
-        $localStorage.carrito = $scope.carrito;
+    $scope.goBack = function() {
+        $window.history.back();
     }
 }
 
 controllers.carritoCtrl = function($scope, $localStorage, $window) {
     $scope.carrito = [];
+    $scope.total = 0;
 
     if ($localStorage.carrito != null) {
         $scope.carrito = $localStorage.carrito;
+        $scope.carrito.forEach(function(p) { 
+            $scope.total += (p.item.precio * p.cantidad);
+        })
     }
 
-    $scope.limpiarCarrito = function(){
-        $scope.carrito = null;
+    $scope.remove = function(item) {
+        let itemIndex = $scope.carrito.indexOf(item);
+        $scope.carrito.splice(itemIndex, 1);
+        $scope.total -= (item.item.precio * item.cantidad);
         $localStorage.carrito = $scope.carrito;
     }
+
+    $scope.limpiarCarrito = function() {
+        $scope.carrito = null;
+        $localStorage.carrito = $scope.carrito;
+        $scope.total = 0;
+    }
+
+    $scope.isShowed = function() {
+        return ($scope.total > 0) ? true : false;
+    }
+    
 }
 
 app.controller(controllers);
